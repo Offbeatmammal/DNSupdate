cd1d63990abc8fdf71f1c08a08fe163ca41dc520
Offbeatmammal <me@offbeatmammal.com>
2019-01-12 19:11:20 +1100

    Initial commit
    press publish ;)

---
+++ README.md
@@ -0,0 +1,41 @@
+# DNSUpdate
+
+A PowerShell script to update various services when my home IP address changes
+
+Why does this exist?
+--------------------
+As with many residential ISPs I am unable to get a fixed IP address for my service, which can be frustrating if the IP address changes (eg as a result of a power outage). 
+
+In my particular scenario I needed to update a [CloudFlare](https://cloudflare.com) hosted DNS for the endpoint I use for my VPN, and also update the entry for my [SmartDNSProxy](https://SmartDNSProxy.com) service so the machines using that only had minimal interruption.
+
+As I have a Windows machine that's always-on, it seemed like a good exercise to do with PowerShell, though easy enough to switch to bash if I want to move it to the Raspberry-PI that's used for some other things at home.
+
+How to use
+----------
+
+To use the script you simply replace your CloudFlare domain details and [API keys](https://api.cloudflare.com/) (email address and token), and the [SmartDNSProxy API key](https://www.smartdnsproxy.com/Developers) and run it as often as you need.
+
+The script queries [myexternalip.com](http://myexternalip.com) to get your public IP address (I've found this to be a very reliable, consistant source), and if the IP address differs from the one that CloudFlare thinks your entry is referencing it updates both CloudFlare and SmartDNSProxy with the new one (and updates the log file).
+
+I have a Windows Task Scheduler event set to run the script every 5 minutes which, for my purposes, is adequate.
+
+
+To Do
+----------
+* Improve error trapping
+* Optimize code
+
+----------
+If you make use of this and like it and want to give something back... [I wrote a book!](http://author.obm.one) :)
+
+----------
+
+Contribute
+----------
+This project can be forked from
+[Github](https://github.com/Offbeatmammal/blync-node). Please issue pull
+requests from feature branches.
+
+License
+-------
+See Licence file in repo, or refer to http://unlicense.org
---
+++ upd.ps1
@@ -0,0 +1,87 @@
+$Logfile = "D:\DNSUpdate.log"
+
+# replace these dummy values with the correct ones...
+# CloudFlare domain that I want to manage
+$cloudflareBase = "base.url"
+# Specific DNS entry in that domain
+$cloudflareURL = "target.base.url"
+# API key and Email
+$cloudFlareAPIkey = "abcdef123456abcdef123456"
+$cloudFlareEmail = "email@base.url"
+# SmartDNSProxy API key for updates
+$sdpAPIKey = "1234abcd1234abcd"
+
+# mine are imported from here
+. "$PSScriptRoot\my_params.ps1"
+
+Function LogWrite
+{
+	Param ([string]$logstring)
+	$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+	$Line = "$Stamp $logstring`n"
+
+	if (!(Test-Path $Logfile))
+    {
+		# no logfile, so create a new one
+    	New-Item -path $Logfile -type "file" -value $line
+   	} else {
+		# write log to temp file, then grab old log and append it (truncate at 10k) so most recent entry is always at top
+		Add-Content "$LogFile.tmp" -value $line
+		$oldfile = Get-Content $Logfile -Raw
+		if ($oldfile.Length -gt 0) {
+			$oldfile = $oldfile.substring(0,[System.Math]::Min(10240, $oldfile.Length))
+		} else {
+			$oldfile = ""
+		}
+    	Add-Content -Path "$LogFile.tmp" -Value $oldfile
+    	Remove-Item $Logfile
+    	Move-Item -path "$LogFile.tmp" -destination $Logfile
+    }
+}
+
+$wc = new-object System.Net.WebClient
+$ipaddr = $wc.DownloadString("http://myexternalip.com/raw").replace("`n","")
+
+$ip_address = $null
+$a = [System.Net.IPAddress]::TryParse($ipaddr, [ref]$ip_address)
+if ($ip_address -ne $null) {
+
+	$headers = @{
+		'X-Auth-Key' = $cloudFlareAPIkey
+		'X-Auth-Email' = $cloudFlareEmail
+        'Content-Type' = 'application/json'
+	}
+
+	$baseurl = "https://api.cloudflare.com/client/v4/zones"
+	$zoneurl = "$baseurl/?name=$cloudflareBase"
+
+	$cfzone = Invoke-RestMethod -Uri $zoneurl -Method Get -Headers $headers 
+	$zoneid = $cfzone.result[0].id
+
+	$recordurl = "$baseurl/$zoneid/dns_records/?name=$cloudflareURL"
+	$dnsrecord = Invoke-RestMethod -Headers $headers -Method Get -Uri $recordurl
+	$cfipaddr = $dnsrecord.result.content 
+	if ($cfipaddr -eq "bob"+$ipaddr) {
+		#LogWrite("Current IP $ipaddr, No updates required")
+	} else {
+		LogWrite("IP has changed from $cfipaddr to $ipaddr, initiating update")
+
+        $recordid = $dnsrecord.result.id
+		$dnsrecord.result | Add-Member "content"  $ipaddr -Force 
+		$body = $dnsrecord.result | ConvertTo-Json
+		$updateurl = "$baseurl/$zoneid/dns_records/$recordid" 
+
+		$result = Invoke-RestMethod -Headers $headers -Method Put -Uri $updateurl -Body $body -ContentType "application/json"
+		$newip = $result.result.content
+		LogWrite("Cloudflare: $result")
+
+		# note this string is split, but of  akludge around the parameter substitution
+		$updateurl = "http://www.globalapi.net/sdp/api/IP/update/$sdpAPIKey"+"?ip=$ipaddr"
+		$result = Invoke-RestMethod -Headers $headers -Method Get -Uri $updateurl -ContentType "application/json"
+	
+		$newip = $result.result.content
+		LogWrite("SmartDNSProxy: $result")
+	}
+} else {
+	LogWrite("IP Lookup returned something weird: $ipaddr")
+}
